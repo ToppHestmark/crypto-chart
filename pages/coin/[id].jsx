1ed32dd724dc60700f791abd7a1df42ecb0281fd
Suphachai Hestmark <top_hestmark@yahoo.no>
2021-05-21 19:42:48 +0200

    Score details UI

--- pages/coin/[id].jsx
+++ pages/coin/[id].jsx
@@ -4,19 +4,52 @@ import styles from "./Coin.module.css";
 const Coin = ({ coin }) => {
   return (
     <Layout>
-      <div className={styles.coin_page}>
-        <div className={styles.coin_container}>
+      <div className={styles.page}>
+        <div className={styles.container}>
+          <span className={styles.rank}> {coin.market_cap_rank} </span>
           <img
             src={coin.image.small}
             alt={coin.name}
-            className={styles.coin_image}
+            className={styles.image}
           />
-          <h1 className={styles.coin_name}> {coin.name} </h1>
-          <p className={styles.coin_ticker}> {coin.symbol} </p>
-          <p className={styles.coin_current}>
+          <h1 className={styles.name}> {coin.name} </h1>
+          <span className={styles.ticker}> {coin.symbol} </span>
+          <p className={styles.current}>
             {" "}
             $ {coin.market_data.current_price.usd}{" "}
           </p>
+
+          <h3 className={styles.score_header}> Score </h3>
+          <div className={styles.bottom}>
+            <span>
+              <p> Coingecko </p>
+              <b> {coin.coingecko_score.toFixed(2)} </b>
+            </span>
+            <span>
+              <p>Developer</p>
+              <b>{coin.developer_score.toFixed(2)}</b>
+            </span>
+            <span>
+              <p>Community</p>
+              <b> {coin.community_score.toFixed(2)} </b>{" "}
+            </span>
+            <span>
+              <p>Liquidity</p>
+              <b> {coin.liquidity_score.toFixed(2)} </b>
+            </span>
+          </div>
+
+          <h3 className={styles.community_header}> Community Data </h3>
+          <div className={styles.bottom}>
+            <span>
+              <p> Twitter followers </p>
+              <b> {coin.community_data.twitter_followers} </b>
+            </span>
+            <span>
+              <p>Reddit subscribers</p>
+              <b>{coin.community_data.reddit_subscribers}</b>
+            </span>
+          </div>
         </div>
       </div>
     </Layout>
